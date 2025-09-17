e9cb53e58062d01f0237542fbd2a108195bdf458
paragdeka <paragdeka@zohomail.in>
2025-09-17 17:27:19 +0000

    feat(server): add route and handlers to serve Ticket details and Message history linked with ticket

--- server/src/controllers/ticket.controller.ts
+++ server/src/controllers/ticket.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import Ticket from "../models/ticket.model";
+import mongoose from "mongoose";
 
 export const listTickets = async (
   _req: Request,
@@ -24,6 +25,50 @@ export const listTicketsWithFirstMessage = async (
       "messages"
     );
     res.json(tickets);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTicket = async (
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid id." });
+    }
+
+    const ticket = await Ticket.findOne({ _id: id }).populate("messages");
+    if (!ticket) return res.status(404).json({ message: "Ticket not found." });
+    res.json(ticket);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTicketMessageHistory = async (
+  req: Request<{ sessionId: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { sessionId } = req.params;
+    if (!sessionId) {
+      return res.status(400).json({ message: "Invalid session id" });
+    }
+
+    const ticket = await Ticket.findOne({ sessionId }).populate({
+      path: "messages",
+      populate: {
+        path: "agentId",
+        model: "Agent",
+      },
+    });
+    if (!ticket) return res.status(404).json({ message: "Ticket not found." });
+    res.json(ticket);
   } catch (error) {
     next(error);
   }
--- server/src/routes/ticket.routes.ts
+++ server/src/routes/ticket.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { listTicketsWithFirstMessage } from "../controllers/ticket.controller";
+import {
+  getTicket,
+  getTicketMessageHistory,
+  listTicketsWithFirstMessage,
+} from "../controllers/ticket.controller";
 import { requireAgentAuth } from "../middlewares/agent-auth.middleware";
 
 const router = Router();
 
 router.get("/", requireAgentAuth, listTicketsWithFirstMessage);
+router.get("/:id", requireAgentAuth, getTicket);
+router.get("/history/:sessionId", getTicketMessageHistory);
 
 export default router;
