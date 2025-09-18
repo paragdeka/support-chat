ff27994c416cc2fdccb476fc555d31fef5f60129
paragdeka <paragdeka@zohomail.in>
2025-09-18 16:36:52 +0000

    feat(client): add notification center for new tickets

--- client/src/app/components/header/header.ts
+++ client/src/app/components/header/header.ts
@@ -3,10 +3,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Profile } from '../profile/profile';
+import { NotificationCenter } from '../notification-center/notification-center';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, Profile],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, Profile, NotificationCenter],
   templateUrl: './header.html',
 })
 export class Header {}
---
+++ client/src/app/components/notification-center/notification-center.ts
@@ -0,0 +1,34 @@
+import { Component, effect, inject, signal } from '@angular/core';
+import { AgentSocketService, UnassignedTicketPayload } from '../../services/agent-socket.service';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-notification-center',
+  imports: [MatMenuModule, MatIconModule, CommonModule],
+  templateUrl: './notification-center.html',
+})
+export class NotificationCenter {
+  agentSocketService = inject(AgentSocketService);
+  newTicket = this.agentSocketService.unassignedTicket;
+  notifications = signal<UnassignedTicketPayload[]>([]);
+  notify = signal<boolean>(false);
+  private router = inject(Router);
+
+  private syncNotifications = effect(() => {
+    if (this.newTicket()) {
+      this.notifications.update((prev) => [...prev, this.newTicket()!]);
+      this.notify.set(true);
+    }
+  });
+
+  notificationSeen() {
+    this.notify.set(false);
+  }
+
+  onNotificationClick(ticketId: string) {
+    this.router.navigate([`/support/ticket/${ticketId}`]);
+  }
+}
